70076d7ff2fab6f3b7dc4bd9f0e06e75a2903f00
stuart-hahn <47613093+stuart-hahn@users.noreply.github.com>
2025-02-23 11:22:18 -0500

    implement lookupHighestDriverAgeFactor

--- socotra-config/plugins/java/SimpleAutoRatingPlugin.java
+++ socotra-config/plugins/java/SimpleAutoRatingPlugin.java
@@ -3,6 +3,8 @@ package com.socotra.deployment.customer;
 import com.socotra.coremodel.*;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -48,7 +50,12 @@ public class SimpleAutoRatingPlugin implements RatePlugin {
         logger.info("rateVehicles vehicles={}", policy.vehicles());
 
         // TODO: Implement applying rateVehicleCoverages() to each vehicle
-        List<RatingItem> allVehicleRates = List.of();
+        List<RatingItem> allVehicleRates = new ArrayList<>();
+
+        // Iterate over all vehicles and apply rating
+        for (Vehicle vehicle : policy.vehicles()) {
+            allVehicleRates.addAll(rateVehicleCoverages(vehicle, policy));
+        }
 
         logger.info("rateVehicles rates={}", allVehicleRates);
         return allVehicleRates;
@@ -57,18 +64,18 @@ public class SimpleAutoRatingPlugin implements RatePlugin {
     /**
      * Calculates rates for all of a Vehicle's coverages
      */
-    private List<RatingItem> rateVehicleCoverages(Vehicle vehicle) {
+    private List<RatingItem> rateVehicleCoverages(Vehicle vehicle, SimpleAuto policy) {
         logger.info("rateVehicleCoverages vehicle={}", vehicle);
 
         List<RatingItem> coverageRates = new ArrayList<>();
 
         // Required Coverages:
-        coverageRates.add(this.rateBodilyInjury(vehicle, vehicle.bodilyInjury()));
-        coverageRates.add(this.ratePropertyDamage(vehicle, vehicle.propertyDamage()));
+        coverageRates.add(this.rateBodilyInjury(vehicle, vehicle.bodilyInjury(), policy));
+        coverageRates.add(this.ratePropertyDamage(vehicle, vehicle.propertyDamage(), policy));
 
         // Optional Coverages:
         if (vehicle.collision() != null) {
-            coverageRates.add(this.rateCollision(vehicle, vehicle.collision()));
+            coverageRates.add(this.rateCollision(vehicle, vehicle.collision(), policy));
         }
         // TODO: Implement rateComprehensive() when applicable
 
@@ -88,11 +95,11 @@ public class SimpleAutoRatingPlugin implements RatePlugin {
     /** Calculate rate for BodilyInjury coverage as
      * = BaseRate * DriverAgeFactor * LimitFactor
      */
-    private RatingItem rateBodilyInjury(Vehicle vehicle, BodilyInjury coverage) {
+    private RatingItem rateBodilyInjury(Vehicle vehicle, BodilyInjury coverage, SimpleAuto policy) {
         logger.info("rateBodilyInjury coverage={}", coverage);
 
         double rate = this.lookupBaseRate(vehicle)
-            * this.lookupHighestDriverAgeFactor()
+            * this.lookupHighestDriverAgeFactor(policy)
             * this.lookupLimitFactor(coverage.limit());
 
         RatingItem ratingItem = RatingItem.builder()
@@ -109,11 +116,11 @@ public class SimpleAutoRatingPlugin implements RatePlugin {
      * Calculate rate for PropertyDamage coverage as
      * = BaseRate * DriverAgeFactor * LimitFactor
      */
-    private RatingItem ratePropertyDamage(Vehicle vehicle, PropertyDamage coverage) {
+    private RatingItem ratePropertyDamage(Vehicle vehicle, PropertyDamage coverage, SimpleAuto policy) {
         logger.info("ratePropertyDamage coverage={}", coverage);
 
         double rate = this.lookupBaseRate(vehicle)
-            * this.lookupHighestDriverAgeFactor()
+            * this.lookupHighestDriverAgeFactor(policy)
             * this.lookupLimitFactor(coverage.limit());
 
         RatingItem ratingItem = RatingItem.builder()
@@ -130,11 +137,11 @@ public class SimpleAutoRatingPlugin implements RatePlugin {
      * Calculate rate for Collision coverage as
      * = BaseRate * DriverAgeFactor * DeductibleFactor
      */
-    private RatingItem rateCollision(Vehicle vehicle, Collision coverage) {
+    private RatingItem rateCollision(Vehicle vehicle, Collision coverage, SimpleAuto policy) {
         logger.info("rateCollision coverage={}", coverage);
 
         double rate = this.lookupBaseRate(vehicle)
-            * this.lookupHighestDriverAgeFactor()
+            * this.lookupHighestDriverAgeFactor(policy)
             * this.lookupDeductibleFactor(coverage.deductible());
 
         RatingItem ratingItem = RatingItem.builder()
@@ -210,7 +217,21 @@ public class SimpleAutoRatingPlugin implements RatePlugin {
     /**
      * Lookup DriverAgeFactor by driver age
      */
-    private double lookupHighestDriverAgeFactor() {
+    private double lookupHighestDriverAgeFactor(SimpleAuto policy) {
+
+        // for every driver on the policy, get their age
+        // take the factor that corresponds with the lowest age
+        // if the factor is higher than the highest factor, update the highestFactor
+
+        double highestFactor = 0.0;
+
+        for (Driver driver : policy.drivers()) {
+            int age = calculateAge(driver);
+            double factor = getDriverAgeFactor(age);
+            highestFactor = Math.max(highestFactor, factor);
+        }
+
+        return highestFactor;
         // TODO: Implement lookup for driver age factor
         // and return the *highest* factor of any driver on the policy
         // ex: if a quote has DriverA=22 and DriverB=41, return 1.75
@@ -222,6 +243,24 @@ public class SimpleAutoRatingPlugin implements RatePlugin {
         // 41-60 => 0.75
         // 61+   => 0.50
 
-        throw new RuntimeException("Not implemented!");
+//        throw new RuntimeException("Not implemented!");
+    }
+
+    private int calculateAge(Driver driver) {
+        return Period.between(driver.data().dateOfBirth(), LocalDate.now()).getYears();
+    }
+
+    private double getDriverAgeFactor(int age) {
+        if (age <= 20) {
+            return 2.5;
+        } else if (age <= 25) {
+            return 1.75;
+        } else if (age <= 40) {
+            return 1.10;
+        } else if (age <= 60) {
+            return 0.75;
+        } else {
+            return 0.5;
+        }
     }
 }
